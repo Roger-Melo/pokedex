46e3d15e075793f33fbd023585faa320f0a8f3fc
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-24 18:38:38 -0300

    Corrige pequenos erros na li

--- app.js
+++ app.js
@@ -15,11 +15,11 @@ const fetchPokemon = () => {
       const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
         const types = pokemon.types.map(typeInfo => typeInfo.type.name);
         accumulator += `
-        <li class="card">
-          <img class="card-image ${types[0]}" alt="${pokemon.name}" src="https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png" />
-          <h2 class=>${pokemon.id}.${pokemon.name}</h2>
+        <li class="card  ${types[0]}">
+          <img class="card-image" alt="${pokemon.name}" src="https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png" />
+          <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
           <p class="card-subtitle">${types.join(' | ')}</p>
-        <li>
+        </li>
         `;
 
         return accumulator;
