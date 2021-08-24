fe34decd70c6ab466faeaf70a3daf6dd6705048e
viniciussantana <viniciusantana@yahoo.com.br>
2021-08-24 18:59:46 -0300

    refatora - coloca pokemon li em um funçao

--- app.js
+++ app.js
@@ -5,26 +5,28 @@ fetch(getPokemonUrl(index+1))
   .then(response => response.json())
 );
 
+const generateHTML = pokemons => {
+  return pokemons.reduce((accumulator, pokemon) => {
+    const types = pokemon.types.map(typeInfo => typeInfo.type.name);
+    accumulator += `
+    <li class="card  ${types[0]}">
+      <img class="card-image" alt="${pokemon.name}" src="https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png" />
+      <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+      <p class="card-subtitle">${types.join(' | ')}</p>
+    </li>
+    `;
+
+    return accumulator;
+  }, '');
+
+  
+}
+
 const fetchPokemon = () => {
  const pokemonPromises = generatePokemonPromises();
 
   Promise.all(pokemonPromises)
-    .then(pokemons => {
-      return pokemons.reduce((accumulator, pokemon) => {
-        const types = pokemon.types.map(typeInfo => typeInfo.type.name);
-        accumulator += `
-        <li class="card  ${types[0]}">
-          <img class="card-image" alt="${pokemon.name}" src="https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png" />
-          <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-          <p class="card-subtitle">${types.join(' | ')}</p>
-        </li>
-        `;
-
-        return accumulator;
-      }, '');
-
-      
-    })
+    .then(generateHTML)
     .then(pokemons => {
       const ul = document.querySelector('[data-js="pokedex"]');
       ul.innerHTML= pokemons;
